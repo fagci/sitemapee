7088b7a54b1b54bf67745ede21628004a9601329
Mikhail Yudin <fagci.nsk@gmail.com>
2021-06-25 10:14:59 +0700

    refactor

--- sitemapee.py
+++ sitemapee.py
@@ -8,18 +8,21 @@ import sys
 from urllib.parse import urlparse
 from urllib.request import urlopen
 
+
 class Crawler:
     A_RE = re.compile(r'<a.*href=\W?(.+?)[\'">\s]')
+
     def __init__(self, uri):
         pu = urlparse(uri)
 
         self.uris = {}
+
+        self.scheme = pu.scheme
         self.netloc = pu.netloc
         self.netloc_ascii = pu.netloc.encode('idna').decode('ascii')
-        self.scheme = pu.scheme
 
-        self.start_uri = '%s://%s%s' % (self.scheme, self.netloc_ascii, pu.path or '/')
         self.root = '%s://%s' % (self.scheme, self.netloc_ascii)
+        self.start_uri = '%s%s' % (self.root, pu.path or '/')
 
     def normalize_uri(self, uri):
         if uri.startswith('//'):
@@ -41,23 +44,22 @@ class Crawler:
 
         with urlopen(uri) as r:
             self.uris[uri] = r.headers.get('Last-Modified')
-            c = r.read().decode()
-            links = self.A_RE.findall(c)
+            html = r.read().decode()
+            unique_links = set(self.A_RE.findall(html))
 
-            normalized_uris = map(self.normalize_uri, set(links))
+            normalized_uris = map(self.normalize_uri, unique_links)
             for u in filter(self.notpassed, normalized_uris):
                 self.crawl(u)
 
 
 class SitemapGenerator:
     DIR = Path(__file__).resolve().parent
-    HEADER = '<?xml version="1.0" encoding="utf-8"?>'
-    URLSET_I = (
-        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" '
-        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
-        'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n'
-    )
-    URLSET_O = '</urlset>'
+    HEADER = '<?xml version="1.0" encoding="utf-8"?>\n'
+    URLSET_I = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    URLSET_O = '</urlset>\n'
+    URL_I = '<url>\n'
+    URL_O = '</url>\n'
+
     def __init__(self):
         self.config = ConfigParser()
         self.config.read(self.DIR / 'config.ini')
@@ -73,11 +75,13 @@ class SitemapGenerator:
         with sitemap.open('w') as s:
             s.write(self.HEADER)
             s.write(self.URLSET_I)
+
             for uri, lastmod in uris.items():
                 for pattern, opts in config.items():
                     if not re.match(pattern, uri):
                         continue
-                    s.write('<url>\n')
+
+                    s.write(self.URL_I)
                     s.write('\t<loc>%s</loc>\n' % uri)
                     changefreq = opts.get('changefreq')
                     priority = float(opts.get('priority', '0.5'))
@@ -86,10 +90,10 @@ class SitemapGenerator:
                         s.write('\t<lastmod>%s</lastmod>\n' % lastmod)
                     if changefreq:
                         s.write('\t<changefreq>%s</changefreq>\n' % changefreq)
-                    s.write('</url>\n')
+                    s.write(self.URL_O)
                     break
-            s.write(self.URLSET_O)
 
+            s.write(self.URLSET_O)
 
 
 def main(uri, file):
@@ -99,7 +103,7 @@ def main(uri, file):
     except KeyboardInterrupt:
         while True:
             answer = input('Save to %s? (y/n) ' % file).lower()
-            if answer not in ('y','n'):
+            if answer not in ('y', 'n'):
                 print('Type y or n.')
                 continue
             if answer == 'n':
@@ -110,4 +114,4 @@ def main(uri, file):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2] if len(sys.argv)==3 else 'sitemap.xml')
+    main(sys.argv[1], sys.argv[2] if len(sys.argv) == 3 else 'sitemap.xml')
